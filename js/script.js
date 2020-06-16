2e46ef3e8bcbea7f6cae6589e605f83e206dbec1
alessandrocapanna <acapanna75@gmail.com>
2020-06-16 16:01:11 +0200

    commiit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-
+var invioMessaggio = $('.bot-bar i:last-child');
+$('.bot-bar input').click(
+  function (){
+    $(invioMessaggio).removeClass('fa-microphone');
+    $(invioMessaggio).addClass('fa-paper-plane');
+  }
+);
+$(invioMessaggio).click(
+  function(){
+    var messaggio = '<li class="messaggi-inviati">' + $('.bot-bar input').val() + '</li>';
+    $('.messaggi ul').append(messaggio);
+  }
+);
