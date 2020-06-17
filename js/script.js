af7fd6d88bc5ae5bb81cd8c6599bb9e65d8fe13a
alessandrocapanna <acapanna75@gmail.com>
2020-06-17 12:06:17 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -2,29 +2,34 @@
 var data = new Date();
 var Hh, Mm;
 Hh = data.getHours() + ":";
-Mm = data.getMinutes();
+Mm = data.getMinutes().toString();
+if (Mm.toString().length < 2) {
+  Mm = '0'+ Mm;
+}
 var ora = Hh + Mm
 
 // NOTE: al click su input cambio icona per invio mex
 var invioMessaggio = $('.bot-bar i:last-child');
 $('.bot-bar input').click(
   function (){
-    $(invioMessaggio).removeClass('fa-microphone');
-    $(invioMessaggio).addClass('fa-paper-plane');
+    $('i.fa-microphone').hide();
+    $('i.fa-paper-plane').show();
   }
 );
 // NOTE: al click su icona invio mex e lui risponde ok
-$(invioMessaggio).click(
+$('i.fa-paper-plane').click(
   function(){
     var messaggioInviato = '<p class="messaggi-inviati">' + $('.bot-bar input').val() + '<span>' + ora + '</span>'+ '</p>';
     $('.messaggi ').append(messaggioInviato);
     $('.bot-bar input').val('');
+    // NOTE: rimetto icona del microfono
+    $('.bot-bar i.fa-paper-plane').hide();
+    $('.bot-bar i.fa-microphone').show();
 
     // NOTE: dopo un secondo risponde ok
     countDown(1);
     function countDown(secondi){
       var secondiCount = setInterval(function(){
-        console.log(secondi);
         if (secondi == 0) {
           clearInterval(secondiCount);
           var messaggioRicevuto = '<p class="messaggi-ricevuti">' + 'ok' + '<span>' + ora + '</span>'+ '</p>';
@@ -36,3 +41,12 @@ $(invioMessaggio).click(
     }
   }
 );
+
+
+// NOTE: per il search
+// var ricercaNome = $('.search input').val();
+// var contatti = [];
+// contatti.push()
+// if ($('.search input').val().includes() ) {
+//
+// }
